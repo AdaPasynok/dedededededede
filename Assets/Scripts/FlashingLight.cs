8a3c37e5d729a98c21358d15436d09bb53408f88
AdaPasynok <109346249+AdaPasynok@users.noreply.github.com>
2024-02-05 11:41:07 +0900

    Наброски уровня Path

---
+++ Assets/Scripts/FlashingLight.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlashingLight : MonoBehaviour
+{
+    private Image blackImage;
+
+    private void Start()
+    {
+        blackImage = GetComponent<Image>();
+        AudioManager.Instance.OnKick += () => blackImage.enabled = !blackImage.enabled;
+    }
+}
